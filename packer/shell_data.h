7dd7636feee50d88e4113ae564ce04577f7c8499
eronana <erona@loli.bz>
2017-04-05 14:15:36 +0800

    update shell_data

--- packer/shell_data.h
+++ packer/shell_data.h
@@ -1,7 +1,6 @@
 #pragma once
-/*
+DEFINE_DWORD(chksum, 0);
 DEFINE_STRING(user32, "user32.dll");
 DEFINE_STRING(MessageBoxA, "MessageBoxA");
-DEFINE_STRING(title, "Hello");
-DEFINE_STRING(content, "This program has been packed!");
-*/
+DEFINE_STRING(title, "Error");
+DEFINE_STRING(content, "Verification failed, this program has been modified!");
